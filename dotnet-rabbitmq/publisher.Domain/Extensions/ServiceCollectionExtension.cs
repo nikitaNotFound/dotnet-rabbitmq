24eb516d454bb3ecbe6b0904cca42196da23394b
Nikita Ivanov <46090847+nikita-a-i@users.noreply.github.com>
2021-09-18 14:13:26 +0300

    Initial commit

---
+++ dotnet-rabbitmq/publisher.Domain/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace publisher.Domain.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddPublisherDomain(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
